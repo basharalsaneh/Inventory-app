3ba7563dae49b7a5e2063ced052bc88b2231ffc4
peko <peko@DESKTOP-AQ4KUUG>
2023-07-03 13:25:11 +0200

    Created logout function

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -119,7 +119,21 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+// Logout User
+const logout = asyncHandler(async (req, res) => {
+  // Remove HTTP-only cookie
+  res.cookie("token", "", {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(0),
+    sameSite: "none",
+    secure: true,
+  });
+  return res.status(200).json({ message: "Successfully Logged Out" });
+});
+
 module.exports = {
   registerUser,
   loginUser,
+  logout,
 };
