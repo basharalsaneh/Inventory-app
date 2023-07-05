6b9f20ffbaa892fff494dbaf1ff9fa6acdeea5c2
peko <peko@DESKTOP-AQ4KUUG>
2023-07-05 11:41:07 +0200

    Created User Profile Route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,11 +3,14 @@ const {
   registerUser,
   loginUser,
   logout,
+  getUser,
 } = require("../controllers/UserController");
+const protect = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logout);
+router.get("/getuser", protect, getUser);
 
 module.exports = router;
