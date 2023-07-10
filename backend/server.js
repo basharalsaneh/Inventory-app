e3d9062ef6e613b5b465be4d8c100f9ead132f52
peko <peko@DESKTOP-AQ4KUUG>
2023-07-10 11:58:25 +0200

    Create Product route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
 const errorHandler = require("./middleware/errorMiddleware");
 const cookieParser = require("cookie-parser");
 
@@ -17,6 +18,7 @@ app.use(bodyParser.json());
 
 // Routes middleware
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 // Routes
 app.get("/", (req, res) => {
