cb9f1c9ae7fd2bd26810ba18a3244add3e72bd94
peko <peko@DESKTOP-AQ4KUUG>
2023-07-10 12:42:23 +0200

    Add new product function

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,52 @@
 const asyncHandler = require("express-async-handler");
+const Product = require("../models/productModel");
+const { fileSizeFormatter } = require("../utils/fileUpload");
+const cloudinary = require("cloudinary");
 
 const createProduct = asyncHandler(async (req, res) => {
-  res.send("Product Created");
+  const { name, sku, category, quantity, price, description } = req.body;
+
+  // Validation
+  if (!name || !category || !quantity || !price || !description) {
+    res.status(400);
+    throw new Error("Please fill in all fields");
+  }
+
+  // Handle Image upload
+  let fileData = {};
+  if (req.file) {
+    // Save Image to Cloudinary
+    let uploadedFile;
+    try {
+      uploadedFile = await cloudinary.uploader.upload(req.file.path, {
+        folder: "Pinvent App",
+        resource_type: "image",
+      });
+    } catch (error) {
+      res.status(500);
+      throw new Error("Image couldn't be uploaded");
+    }
+
+    fileData = {
+      fileName: req.file.originalname,
+      filePath: uploadedFile.secure_url,
+      fileType: req.file.mimetype,
+      fileSize: fileSizeFormatter(req.file.size, 2),
+    };
+  }
+
+  // Create Product
+  const product = await Product.create({
+    user: req.user.id,
+    name,
+    sku,
+    category,
+    price,
+    description,
+    quantity,
+    image: fileData,
+  });
+  res.status(201).json(product);
 });
 
 module.exports = {
