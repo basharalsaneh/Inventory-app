a97f61e89e4782f25743c6e9a8a78c5dfc96c32c
peko <peko@DESKTOP-AQ4KUUG>
2023-08-03 09:32:52 +0200

    Fixed login, registration and logout functions in the frontend and dashboard.

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -3,37 +3,45 @@ import { RiProductHuntLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import "./Home.scss";
 import heroImg from "../../assets/inv-img.png";
+import { ShowOnLogin, ShowOnLogout } from "../../components/protect/HiddenLink";
 
 const Home = () => {
   return (
     <div className="home">
-      <nav className="container --flex-between">
+      <nav className="container --flex-between ">
         <div className="logo">
           <RiProductHuntLine size={35} />
         </div>
+
         <ul className="home-links">
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <button className="--btn --btn-primary">
-              <Link to="/login">Login</Link>
-            </button>
-          </li>
-          <li>
-            <button className="--btn --btn-primary">
-              <Link to="/dashboard">Dashboard</Link>
-            </button>
-          </li>
+          <ShowOnLogout>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ShowOnLogout>
+          <ShowOnLogout>
+            <li>
+              <button className="--btn --btn-primary">
+                <Link to="/login">Login</Link>
+              </button>
+            </li>
+          </ShowOnLogout>
+          <ShowOnLogin>
+            <li>
+              <button className="--btn --btn-primary">
+                <Link to="/dashboard">Dashboard</Link>
+              </button>
+            </li>
+          </ShowOnLogin>
         </ul>
       </nav>
-      {/* Hero Section */}
+      {/* HERO SECTION */}
       <section className="container hero">
         <div className="hero-text">
           <h2>Inventory {"&"} Stock Management Solution</h2>
           <p>
-            Inventory system to control and manage products in the warehouse in
-            real timeend integrated to make it easier to develop your business.
+            Inventory system to control and manage proucts in the warehouse in
+            real timeand integrated to make it easier to develop your business.
           </p>
           <div className="hero-buttons">
             <button className="--btn --btn-secondary">
